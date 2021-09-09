aaa70aff45e9433c08153c9de43c9e1c82f9ea32
Zhanarbek Osmonaliev <zhanarbek.osmonaliev@stonybrook.edu>
2021-09-09 15:22:51 -0400

    Archive landing page, clear landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,23 +5,17 @@ import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons";
 import Canvas from "../three-js/canvas";
 import { makeStyles } from "@material-ui/core/styles";
 import Navbar from "../components/appBar";
-import { Grid, SvgIcon, Hidden } from "@material-ui/core";
+import { Grid, SvgIcon, Hidden, Container } from "@material-ui/core";
 import { Box, Book, Terminal, X } from "react-feather";
 const useStyles = makeStyles({
-  text: {
+  // styles here
+  textDisplay: {
+    fontFamily: "PP Neue Machine, Inter, sans-serif",
     fontSize: "36px",
-    fontWeight: "600",
-    marginTop: "24px",
+    lineHeight: "44px",
     "@media(min-width: 768px)": {
-      fontSize: "48px",
-    },
-  },
-  subText: {
-    fontSize: "30px",
-    fontWeight: "600",
-    marginTop: "24px",
-    "@media(min-width: 768px)": {
-      fontSize: "36px",
+      fontSize: "56px",
+      lineHeight: "64px",
     },
   },
 });
@@ -73,162 +67,53 @@ function HelmetMeta({ website, ...props }) {
   );
 }
 
-function About(props) {
-  let parallax = null;
+const Content = () => {
   const classes = useStyles();
   return (
-    <Parallax
-      ref={(ref) => (parallax = ref)}
-      vertical
-      scrolling={false}
-      pages={3}
-      id="parallax-wrapper"
-    >
+    <Parallax pages={2}>
       <ParallaxLayer
         offset={0}
-        speed={1.0}
-        // onClick={() => parallax.scrollTo(1)}
+        speed={1}
+        style={{
+          display: "flex",
+          width: "100%",
+          left: "10px",
+          right: "10px",
+          maxWidth: "1140px",
+          marginLeft: "auto",
+          marginRight: "auto",
+          alignItems: "center",
+        }}
       >
-        <div className={"parallax-container"}>
-          <div>
-            <span className="icon-wrapper">
-              <SvgIcon fontSize={"default"}>
-                <Box color="white"></Box>
-              </SvgIcon>
-            </span>
-            <span className={"label"}>WELCOME</span>
-            <h1 className={classes.text}>
-              Hi there! <br /> I am{" "}
-              <span className="text-highlight">Zhanar</span>, <br /> a web
-              developer 🚀
-            </h1>
-          </div>
-        </div>
+        <p className={classes.textDisplay}>
+          Hi, I am Zhanar. <br /> I <span>develop</span> user-friendly products.
+        </p>
       </ParallaxLayer>
       <ParallaxLayer
-        offset={0.9}
-        speed={0.5}
-        onClick={() => parallax.scrollTo(1)}
+        offset={0.5}
+        speed={2}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
-        <span className="icon-wrapper passive">
-          <SvgIcon fontSize={"default"}>
-            <Book color="white"></Book>
-          </SvgIcon>
-        </span>
-        <span className={"label passive"}>EDUCATION</span>
-        <br></br>
-        <div className="label-line"></div>
-      </ParallaxLayer>
-      <ParallaxLayer offset={1} speed={1}>
-        <div className={"parallax-container"}>
-          <div>
-            <span className="icon-wrapper">
-              <SvgIcon fontSize={"default"}>
-                <Book color="white"></Book>
-              </SvgIcon>
-            </span>
-            <span className={"label"}>EDUCATION</span>
-            <h1 className={classes.subText}>
-              Currently pursuing my degree in{" "}
-              <span className="text-highlight">Computer Science</span> with HCI
-              specialization from Stony Brook University 🎓
-            </h1>
-          </div>
-        </div>
-      </ParallaxLayer>
-      <ParallaxLayer
-        offset={1.9}
-        speed={0.5}
-        onClick={() => parallax.scrollTo(2)}
-      >
-        <span className="icon-wrapper passive">
-          <SvgIcon fontSize={"default"}>
-            <Terminal color="white"></Terminal>
-          </SvgIcon>
-        </span>
-        <span className={"label passive"}>SKILLS</span>
-        <br></br>
-        <div className="label-line"></div>
-      </ParallaxLayer>
-
-      <ParallaxLayer
-        offset={2}
-        speed={1.0}
-        onClick={() => parallax.scrollTo(0)}
-      >
-        <div className={"parallax-container"}>
-          <div>
-            <span className="icon-wrapper">
-              <SvgIcon fontSize={"default"}>
-                <Terminal color="white"></Terminal>
-              </SvgIcon>
-            </span>
-            <span className={"label"}>SKILLS</span>
-            <h1 className={classes.subText}>
-              I am very passionate about{" "}
-              <span className="text-highlight">web development</span> and{" "}
-              <span className="text-highlight">design</span> 💻
-            </h1>
-            <p className="text-secondary">Languages</p>
-            <button className="skills-button">JavaScript</button>
-            <button className="skills-button">Python</button>
-            <button className="skills-button">Java</button>
-            <p className="text-secondary">Frameworks, Other</p>
-            <button className="skills-button">ReactJS</button>
-            <button className="skills-button">GatsbyJS</button>
-            <button className="skills-button">ThreeJS</button>
-            <button className="skills-button">Material UI</button>
-            <button className="skills-button">Bootstrap</button>
-            <button className="skills-button">Git</button>
-            <button className="skills-button">Webpack</button>
-            <button className="skills-button">react-spring</button>
-            <p className="text-secondary">Software</p>
-            <button className="skills-button">Figma</button>
-            <button className="skills-button">Adobe Creative Cloud</button>
-          </div>
-        </div>
-      </ParallaxLayer>
-      <ParallaxLayer
-        offset={2.9}
-        speed={0.5}
-        onClick={() => parallax.scrollTo(0)}
-      >
-        <span className={"icon-wrapper passive-cancel "}>
-          <SvgIcon fontSize={"default"}>
-            <X color="white"></X>
-          </SvgIcon>
-        </span>
-        <span className={"label passive "}>BACK TO THE TOP</span>
-        <br></br>
-        <div className={"label-line cancel"}></div>
+        <p>Scroll down</p>
       </ParallaxLayer>
     </Parallax>
   );
-}
+};
 
 const IndexPage = ({ data: { site } }) => {
-  let theme = false;
-  if (typeof localStorage !== `undefined`) {
-    theme = localStorage.theme === "dark";
-  }
-  const [themeDark, setTheme] = useState(theme);
+  // const [themeDark, setTheme] = useState(theme);
   return (
     <div>
       <HelmetMeta website={site} />
       <div id="navbar-wrapper">
-        <Navbar toggleTheme={(e) => setTheme(e)}></Navbar>
+        <Navbar></Navbar>
       </div>
       <div id="landing-wrapper">
-        <Grid container spacing={0} justify="center">
-          <Grid item lg={5} xs={12} md={6}>
-            <About></About>
-          </Grid>
-          <Hidden smDown>
-            <Grid item lg={7} xs={12} md={6}>
-              <Canvas darkTheme={themeDark}></Canvas>
-            </Grid>
-          </Hidden>
-        </Grid>
+        <Content />
       </div>
     </div>
   );
