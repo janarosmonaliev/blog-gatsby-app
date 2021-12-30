39289f9bc397db3e3f2d775e83be37141a276f6c
Zhanarbek Osmonaliev <zhanarbek.osmonaliev@stonybrook.edu>
2021-12-30 01:10:44 -0500

    Add More Module

--- src/components/misc-module.js
+++ src/components/misc-module.js
@@ -1,89 +1,109 @@
 import React from "react";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
-import { Grid, Box, Paper, IconButton, SvgIcon } from "@material-ui/core";
+import { Grid, Box, Paper } from "@material-ui/core";
+import UnsplashCard from "../images/unsplash-card.jpg";
+import FStudioCard from "../images/fstudio-card.jpg";
 
 const useStyles = makeStyles({
+  styledCard: {
+    minHeight: "350px",
+    width: "100%",
+    display: "flex",
+    flexWrap: "wrap",
+    boxSizing: "border-box",
+    flexDirection: "column",
+    justifyContent: "flex-end",
+  },
+  styledCardContent: {
+    backgroundImage: "linear-gradient(rgba(0,0,0,0), rgba(0,0,0,1))",
+    padding: "2rem 1rem 1.5rem 1rem",
+    borderRadius: "0 0 20px 20px",
+  },
+  cardTitle: {
+    marginBottom: "0.4rem",
+  },
   cardText: {
-    fontSize: "16px",
-    lineHeight: "24px",
+    fontSize: "15px",
+    color: "#d3d3d3",
+    fontWeight: 300,
+    lineHeight: "20px",
   },
-  halfSingleLeft: {
-    borderRadius: "30px 30px 0 0",
-    "@media(min-width: 960px)": {
-      borderRadius: "30px 0 0 30px",
-    },
+  cardActionText: {
+    fontSize: "15px",
+    marginBottom: "0px",
+    color: "#b9b9b9",
+    display: "inline-block",
   },
-  halfSingleRight: {
-    borderRadius: "0 0 30px 30px",
-    "@media(min-width: 960px)": {
-      borderRadius: "0 30px 30px 0",
-    },
+  unsplashCard: {
+    backgroundImage: `url(${UnsplashCard})`,
+    backgroundSize: "cover",
+    backgroundPosition: "top center",
   },
-  halfFirstLeft: {
-    borderRadius: "30px 30px 0 0",
-    "@media(min-width: 960px)": {
-      borderRadius: "30px 0 0 0",
-    },
-  },
-  halfFirstRight: {
-    borderRadius: "0",
-    "@media(min-width: 960px)": {
-      borderRadius: "0 30px 0 0",
-    },
-  },
-  middle: {
-    borderRadius: "0",
-  },
-  halfLastLeft: {
-    borderRadius: "0",
-    "@media(min-width: 960px)": {
-      borderRadius: "0 0 0 30px",
-    },
-  },
-  halfLastRight: {
-    borderRadius: "0 0 30px 30px",
-    "@media(min-width: 960px)": {
-      borderRadius: "0 0 30px 0",
-    },
-  },
-  wholeFirst: {
-    borderRadius: "30px 30px 0 0",
-  },
-  wholeLast: {
-    borderRadius: "0 0 30px 30px",
+  fstudioCard: {
+    backgroundImage: `url(${FStudioCard})`,
+    backgroundSize: "cover",
+    backgroundPosition: "top center",
   },
 });
 
 const StyledPaper = withStyles({
   root: {
-    minHeight: "450px",
-    padding: "2rem 1rem 2rem 1rem",
-    color: "inherit",
-    "@media(min-width: 960px)": {
-      padding: "2rem",
-    },
+    color: "white",
+    borderRadius: "20px",
+    overflow: "hidden",
   },
 })(Paper);
 
-const StyledLink = (props) => {
+const StyledCard = (props) => {
+  const classes = useStyles();
   return (
-    <IconButton
-      aria-label="link"
-      size="medium"
-      className="link-icon-landing"
-      href={props.href}
-      target="_blank"
-      rel="noreferrer"
-    >
-      <SvgIcon fontSize="small">{props.icon}</SvgIcon>
-    </IconButton>
+    <div>
+      <StyledPaper elevation={0} className={props.class}>
+        <div className={classes.styledCard}>
+          <div className={classes.styledCardContent}>
+            <h2 className={classes.cardTitle}>{props.title}</h2>
+            <p className={classes.cardText}>{props.description}</p>
+            <a
+              className={classes.cardActionText}
+              rel="noreferrer"
+              href={props.href}
+              target="_blank"
+            >
+              {props.linkText} {"->"}
+            </a>
+          </div>
+        </div>
+      </StyledPaper>
+    </div>
   );
 };
 
 const MiscModule = () => {
+  const classes = useStyles();
   return (
     <>
-      <Grid container spacing={1}></Grid>
+      <Grid container spacing={1}>
+        <Grid item xs={12} md={4}>
+          <StyledCard
+            class={classes.unsplashCard}
+            title="Shot On iPhone Series"
+            description="My gallery of photos shot and edited on iPhone X."
+            href="https://unsplash.com/@janarosmonaliev"
+            linkText="unsplash.com"
+          ></StyledCard>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <StyledCard
+            class={classes.fstudioCard}
+            title="F Studio Web Design"
+            description="Collaboration with an e-commerce platform for emerging designers and artists."
+            href="https://fstudionyc.com"
+            linkText="fstudionyc.com"
+          ></StyledCard>
+        </Grid>
+
+        <Grid item xs={12} md={4}></Grid>
+      </Grid>
     </>
   );
 };
